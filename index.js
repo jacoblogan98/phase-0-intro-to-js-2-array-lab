92ed4786a994934481a8029d95908784aebb2b60
Jacob Logan <jacoblogan98@gmail.com>
2022-03-15 14:03:04 -0400

    commit tree 428d381fa325daba858d28dd0d09dcd5b5967127

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat() {
+    const newCats = [...cats, "Broom"];
+    return newCats;
+}
+
+function prependCat() {
+    const newCats = ["Arnold", ...cats];
+    return newCats;
+}
+
+function removeLastCat() {
+    return cats.slice(0,2);
+}
+
+function removeFirstCat() {
+    return cats.slice(-2);
+}
